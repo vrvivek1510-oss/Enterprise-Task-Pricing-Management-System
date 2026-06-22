1b8fffdc6f9bb20911b6a57cd0b25b9364e2d7a5
vrvivek1510-oss <vr.vivek.1510@gmail.com>
2026-06-22 23:29:40 +0530

    API-Changes
    New-API-Changes

---
+++ API/src/main/java/com/web/enterprise_task_pricing/EnterpriseTaskPricingApplication.java
@@ -0,0 +1,13 @@
+package com.web.enterprise_task_pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseTaskPricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseTaskPricingApplication.class, args);
+	}
+
+}
---
+++ API/src/test/java/com/web/enterprise_task_pricing/EnterpriseTaskPricingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.enterprise_task_pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseTaskPricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
