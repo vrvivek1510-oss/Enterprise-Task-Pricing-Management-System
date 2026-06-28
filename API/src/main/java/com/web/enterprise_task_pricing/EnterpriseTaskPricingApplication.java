54a420155b660b942e5849c3104df299f07a9e4a
vrvivek443 <vivek@sixofour.in>
2026-06-28 14:28:29 +0530

    Changes

--- API/src/main/java/com/web/enterprise_task_pricing/EnterpriseTaskPricingApplication.java
+++ API/src/main/java/com/web/enterprise_task_pricing/EnterpriseTaskPricingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EnterpriseTaskPricingApplication {
-
+    //vivek project
 	public static void main(String[] args) {
 		SpringApplication.run(EnterpriseTaskPricingApplication.class, args);
 	}
